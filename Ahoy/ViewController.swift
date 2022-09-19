f4b0cfc057eecc354004530f728227247f641aa0
Sam Johnson <sam@scj.me>
2022-09-19 11:52:57 -0500

    Initial Commit

---
+++ Ahoy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ahoy
+//
+//  Created by Sam Johnson on 9/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
